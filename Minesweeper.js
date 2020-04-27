6a8ed66916d111eb8a368f333bf695e69d6ab27f
lucasvuong <59907180+lucasvuong@users.noreply.github.com>
2020-04-27 11:50:07 -0700

    making controls and board

--- Minesweeper.js
+++ Minesweeper.js
@@ -1,7 +1,36 @@
+let board = [];
+
 function setup() {
     createCanvas(400, 400);
+    for (let r = 0; r < 10; r++) {
+        board[r] = [];
+        for (let c = 0; c < 10; c++) {
+            board[r][c] = new Cell(r * 40, c * 40, "nothing");
+        }
+    }
 }
 
 function draw() {
-    background(51);
+    background(200);
+    board.forEach((element) => {
+        element.forEach((element) => {
+            element.show();
+        });
+    });
+}
+
+function mousePressed() {
+    board.forEach((element) => {
+        element.forEach((element) => {
+            if (
+                element.x <= mouseX &&
+                element.x + 40 >= mouseX &&
+                element.y <= mouseY &&
+                element.y + 40 >= mouseY
+            ) {
+                element.clicked = true;
+                return;
+            }
+        });
+    });
 }
