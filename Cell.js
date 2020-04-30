949496057b53a0034b1e4f03da65e769c35035f0
lucasvuong <59907180+lucasvuong@users.noreply.github.com>
2020-04-29 23:35:00 -0700

    changing color theme

--- Cell.js
+++ Cell.js
@@ -13,11 +13,38 @@ class Cell {
         } else {
             if (this.type >= 1) {
                 push();
-                colorMode(HSB);
-                fill(this.type * 36, 100, 100);
-                //textFont("Courier");
-                //textAlign(CENTER, CENTER);
-                //textSize(40);
+                switch (this.type) {
+                    case 1:
+                        fill(0, 0, 255);
+                        break;
+                    case 2:
+                        fill(0, 255, 0);
+                        break;
+                    case 3:
+                        fill(255, 0, 0);
+                        break;
+                    case 4:
+                        fill(0, 0, 255);
+                        break;
+                    case 5:
+                        fill(185, 111, 237);
+                        break;
+                    case 6:
+                        fill(165, 224, 242);
+                        break;
+                    case 7:
+                        fill(0, 0, 0);
+                        break;
+                    case 8:
+                        fill(99, 99, 99);
+                        break;
+                    case 9:
+                        fill(255, 255, 255);
+                        break;
+                }
+                textFont("Courier");
+                textAlign(CENTER, CENTER);
+                textSize(20);
                 text(this.type, this.x + 20, this.y + 20);
                 pop();
             } else if (this.type == -1) {
